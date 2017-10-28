fc0ee4e0270df6bdc8065853d29ad3e9b662cd01
dchristian3188 <dchristian3188@gmail.com>
2017-10-28 11:22:53 -0700

    workign filter

--- dcmc.web/ClientApp/app/components/videoinfo/videinfo.component.ts
+++ dcmc.web/ClientApp/app/components/videoinfo/videinfo.component.ts
@@ -8,10 +8,30 @@ import { Http } from '@angular/http';
 export class VideoInfoComponent {
     public videos: VideoInfo[];
 
+    public filteredVideos: VideoInfo[];
+
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
         http.get('http://localhost:22368/api/videoinfo').subscribe(result => {
             this.videos = result.json() as VideoInfo[];
+            this.filteredVideos = this.videos;
         }, error => console.error(error));
+    }
+
+    _filterString: string;
+
+    get filterString(): string {
+        return this._filterString;
+    }
+
+    set filterString(value: string) {
+        this._filterString = value;
+        this.filteredVideos = value ? this.filterVideos(value) : this.videos;
+    }
+
+    filterVideos(filter: string): VideoInfo[] {
+        filter = filter.toLocaleLowerCase();
+        return this.videos.filter((video: VideoInfo) =>
+            video.filePath.toLocaleLowerCase().indexOf(filter) !== -1);
     }
 }
 
